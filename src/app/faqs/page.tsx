d0dc425f9b0dc09e6534ecc020ffdb44efebc31a
Jinius <0xjinius@gmail.com>
2025-03-10 19:10:04 +0100

    feat: email support

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -1,31 +1,32 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import FaqPage from "../components/faqs";
+import SupportForm from "../support/page";
 
 export default function FAQs() {
-  return (
-    <div className="container mx-auto px-4">
-        <h1 className="flex justify-center text-4xl p-8 font-bold mb-6">
-          FAQs & Support
-        </h1>
-        
-        <div className="max-w-5xl mx-auto mt-6 px-4">
-        <Tabs defaultValue="faqs" className="max-w-5xl mx-auto mt-6">
-          <TabsList className="grid grid-cols-2 md:grid-cols-2 w-full gap-2">
-            <TabsTrigger value="faqs">
-              Requently Asked Questions
-            </TabsTrigger>
-            <TabsTrigger value="support">Contact Our Support Team</TabsTrigger>
-          </TabsList>
+    return (
+        <div className="container mx-auto px-4">
+            <h1 className="flex justify-center text-4xl p-8 font-bold mb-6">
+                FAQs & Support
+            </h1>
 
-          <TabsContent value="faqs">
-            <FaqPage />
-          </TabsContent>
-          
-          <TabsContent value="support">
-            {/* <NFTCoins /> */}
-          </TabsContent>
-        </Tabs>
+            <div className="max-w-5xl mx-auto mt-6 px-4">
+                <Tabs defaultValue="faqs" className="max-w-5xl mx-auto mt-6">
+                    <TabsList className="grid grid-cols-2 md:grid-cols-2 w-full gap-2">
+                        <TabsTrigger value="faqs">
+                            FAQs
+                        </TabsTrigger>
+                        <TabsTrigger value="support">Contact Support</TabsTrigger>
+                    </TabsList>
+
+                    <TabsContent value="faqs">
+                        <FaqPage />
+                    </TabsContent>
+
+                    <TabsContent value="support">
+                        <SupportForm />
+                    </TabsContent>
+                </Tabs>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
